dfcec4134635e164de545eeb060e44338acd0423
ObNitram <mart1.guillemot@gmail.com>
2023-12-19 23:10:26 +0800

    Correct first frame test

--- test/first_frame_test.dart
+++ test/first_frame_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:forus/pages/navigation_thing.dart';
+import 'package:forus/widget/bottom_nav.dart';
 
 void main() {
   testWidgets('Must be able to trigger a first frame',
@@ -8,7 +8,7 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
       const MaterialApp(
-        home: Navigation(),
+        home: CustomBottomNav(),
       ),
     );
   });
