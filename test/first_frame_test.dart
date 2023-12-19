51b2d1d45481d72b1403705ab18d7521ea3386b9
ObNitram <mart1.guillemot@gmail.com>
2023-12-19 23:07:42 +0800

    Correct first frame test name

--- test/first_frame_test.dart
+++ test/first_frame_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:forus/pages/navigation_thing.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Must be able to trigger a first frame',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
       const MaterialApp(
