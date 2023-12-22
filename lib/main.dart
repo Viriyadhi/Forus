f0d1643534feecca29214eb804350d4acbb16055
ObNitram <mart1.guillemot@gmail.com>
2023-12-23 01:39:33 +0800

    Update the CI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:forus/pages/login_page.dart';
-// import 'package:forus/pages/login_page.dart';
-import 'package:forus/widget/bottom_nav.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
