92b8edfe979dd8e8011970642b29e1ac4b36099b
Viriyadhi <viriyadhitjan@gmail.com>
2023-12-31 02:16:55 +0800

    feat[VS]: scrollable card

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -22,30 +22,32 @@ class _ProfilePageState extends State<ProfilePage> {
             child: SizedBox(
               width: 300,
               height: 550,
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 120.0),
-                  child: Column(
-                    children: [
-                      Text("Username goes Here"),
-                      Text("Email goes Here"),
-                      Text("Favorite tags Here"),
-                      Card(
-                        color: Color.fromRGBO(20, 17, 33, 100),
-                        child: SizedBox(
-                          height: 100,
-                          width: 200,
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 120.0),
+                    child: Column(
+                      children: [
+                        Text("Username goes Here"),
+                        Text("Email goes Here"),
+                        Text("Favorite tags Here"),
+                        Card(
+                          color: Color.fromRGBO(20, 17, 33, 100),
+                          child: SizedBox(
+                            height: 100,
+                            width: 200,
+                          ),
                         ),
-                      ),
-                      Text("Stats"),
-                      Card(
-                        color: Color.fromRGBO(20, 17, 33, 100),
-                        child: SizedBox(
-                          height: 100,
-                          width: 200,
+                        Text("Stats"),
+                        Card(
+                          color: Color.fromRGBO(20, 17, 33, 100),
+                          child: SizedBox(
+                            height: 300,
+                            width: 200,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
