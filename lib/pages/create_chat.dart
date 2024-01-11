789b726d4c109e37924f1a4415d9a73d2a1af168
Viriyadhi <viriyadhitjan@gmail.com>
2024-01-12 02:49:23 +0800

    chore[VS]: installing necessary dependencies

--- lib/pages/create_chat.dart
+++ lib/pages/create_chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class CreateChat extends StatefulWidget {
   final String groupName;
@@ -9,14 +10,156 @@ class CreateChat extends StatefulWidget {
 }
 
 class _CreateChatState extends State<CreateChat> {
+  List<String> selectedValues = [];
+  List<String> items = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(40, 40, 45, 0.612),
       appBar: AppBar(
         title: const Text("Create Thread"),
       ),
-      body: const Center(
-        child: Text("Create Thread"),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 1200,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    hintText: "Thread Title",
+                  ),
+                ),
+              ),
+              Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text('Choose Tags:'),
+                    Theme(
+                      data: ThemeData(
+                        highlightColor:
+                            Colors.transparent, // Remove the highlight color
+                        focusColor:
+                            Colors.transparent, // Remove the focus color
+                      ),
+                      child: TypeAheadFormField<String>(
+                        textFieldConfiguration: TextFieldConfiguration(
+                          decoration: InputDecoration(
+                            labelText: 'Enter tag', // Add this line
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            suffixIcon: const Icon(Icons.arrow_drop_down),
+                          ),
+                        ),
+                        suggestionsCallback: (pattern) {
+                          return items.where((item) => item
+                              .toLowerCase()
+                              .contains(pattern.toLowerCase()));
+                        },
+                        itemBuilder: (context, suggestion) {
+                          return ListTile(
+                            title: Text(suggestion),
+                          );
+                        },
+                        transitionBuilder:
+                            (context, suggestionsBox, controller) {
+                          return suggestionsBox;
+                        },
+                        onSuggestionSelected: (suggestion) {
+                          setState(() {
+                            selectedValues.add(suggestion);
+                          });
+                        },
+                        noItemsFoundBuilder: (context) =>
+                            const SizedBox.shrink(),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      children: selectedValues
+                          .map((value) => Chip(
+                                label: Text(
+                                  value,
+                                  style: const TextStyle(fontSize: 11.0),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 7.0, vertical: 1.0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100.0),
+                                ),
+                                labelPadding: const EdgeInsets.all(1.0),
+                                visualDensity: const VisualDensity(
+                                    horizontal: 0.0, vertical: -4.0),
+                                deleteIcon: Transform.scale(
+                                  scale: 0.7,
+                                  child: const Icon(
+                                    Icons.close,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onDeleted: () {
+                                  setState(() {
+                                    selectedValues.remove(value);
+                                  });
+                                },
+                              ))
+                          .toList(),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 1200,
+                child: TextField(
+                  minLines: 6,
+                  maxLines: 6,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    hintText: "Tell us your thoughts!",
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 1200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(6.0), // Set the border radius
+                    ),
+                  ),
+                  onPressed: () {
+                    // Implement the logic to create the thread
+                    // This is just a placeholder, replace it with your logic
+                    print("Create Thread Button Pressed");
+                  },
+                  child: const Text("Create Thread",
+                      style: TextStyle(color: Color.fromRGBO(40, 40, 45, 1))),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/create_group.dart
+++ lib/pages/create_group.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_database/firebase_database.dart';
+// import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:forus/model/identification_system.dart';
+// import 'package:forus/model/identification_system.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class CreateGroup extends StatefulWidget {
   const CreateGroup({super.key});
@@ -10,111 +11,157 @@ class CreateGroup extends StatefulWidget {
 }
 
 class _CreateGroupState extends State<CreateGroup> {
-  final groupNameInput = TextEditingController();
-  final groupDescriptionInput = TextEditingController();
-
-  Future<void> inputData(String fireGroupName, String fireDescription) async {
-    String groupId =
-        (await IdSystem.getUniqueId("public_data/groups/", "group_id"))
-            .toString();
-    DatabaseReference ref =
-        FirebaseDatabase.instance.ref("public_data/groups/");
-
-    await ref.push().set({
-      "group_name": fireGroupName,
-      "group_desc": fireDescription,
-      "group_id": groupId
-    });
-  }
+  List<String> selectedValues = [];
+  List<String> items = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Hello Ini Bikin Grup"),
+      backgroundColor: const Color.fromRGBO(40, 40, 45, 0.612),
+      appBar: AppBar(
+        title: const Text("Create Group"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 1200,
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    hintText: "Thread Title",
+                  ),
+                ),
+              ),
+              Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text('Choose Tags:'),
+                    Theme(
+                      data: ThemeData(
+                        highlightColor:
+                            Colors.transparent, // Remove the highlight color
+                        focusColor:
+                            Colors.transparent, // Remove the focus color
+                      ),
+                      child: TypeAheadFormField<String>(
+                        textFieldConfiguration: TextFieldConfiguration(
+                          decoration: InputDecoration(
+                            labelText: 'Enter tag', // Add this line
+                            filled: true,
+                            fillColor: Colors.grey[200],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(6.0),
+                            ),
+                            suffixIcon: const Icon(Icons.arrow_drop_down),
+                          ),
+                        ),
+                        suggestionsCallback: (pattern) {
+                          return items.where((item) => item
+                              .toLowerCase()
+                              .contains(pattern.toLowerCase()));
+                        },
+                        itemBuilder: (context, suggestion) {
+                          return ListTile(
+                            title: Text(suggestion),
+                          );
+                        },
+                        transitionBuilder:
+                            (context, suggestionsBox, controller) {
+                          return suggestionsBox;
+                        },
+                        onSuggestionSelected: (suggestion) {
+                          setState(() {
+                            selectedValues.add(suggestion);
+                          });
+                        },
+                        noItemsFoundBuilder: (context) =>
+                            const SizedBox.shrink(),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 4.0,
+                      children: selectedValues
+                          .map((value) => Chip(
+                                label: Text(
+                                  value,
+                                  style: const TextStyle(fontSize: 11.0),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 7.0, vertical: 1.0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100.0),
+                                ),
+                                labelPadding: const EdgeInsets.all(1.0),
+                                visualDensity: const VisualDensity(
+                                    horizontal: 0.0, vertical: -4.0),
+                                deleteIcon: Transform.scale(
+                                  scale: 0.7,
+                                  child: const Icon(
+                                    Icons.close,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                onDeleted: () {
+                                  setState(() {
+                                    selectedValues.remove(value);
+                                  });
+                                },
+                              ))
+                          .toList(),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 1200,
+                child: TextField(
+                  minLines: 6,
+                  maxLines: 6,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.all(20.0),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    hintText: "Tell us your thoughts!",
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 1200,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(6.0), // Set the border radius
+                    ),
+                  ),
+                  onPressed: () {
+                    // Implement the logic to create the thread
+                    // This is just a placeholder, replace it with your logic
+                    print("Create Thread Button Pressed");
+                  },
+                  child: const Text("Create Thread",
+                      style: TextStyle(color: Color.fromRGBO(40, 40, 45, 1))),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: ListView(
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: TextField(
-                    controller: groupNameInput,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: 'Group Name',
-                      labelStyle: const TextStyle(color: Colors.black),
-                      focusedBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: TextField(
-                    controller: groupDescriptionInput,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: 'Description',
-                      labelStyle: const TextStyle(color: Colors.black),
-                      focusedBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextButton(
-                      onPressed: () {
-                        if (groupNameInput.text.isEmpty ||
-                            groupDescriptionInput.text.isEmpty) {
-                          showDialog(
-                              context: context,
-                              builder: (BuildContext context) => AlertDialog(
-                                    title: const Text("Input Text"),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.pop(context, 'Cancel'),
-                                        child: const Text('Cancel'),
-                                      ),
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.pop(context, 'OK'),
-                                        child: const Text('OK'),
-                                      ),
-                                    ],
-                                  ));
-                        }
-                        if (groupNameInput.text.isNotEmpty &&
-                            groupDescriptionInput.text.isNotEmpty) {
-                          inputData(
-                              groupNameInput.text, groupDescriptionInput.text);
-                          groupNameInput.text = "";
-                          groupDescriptionInput.text = "";
-                        }
-                      },
-                      style: TextButton.styleFrom(
-                          fixedSize: const Size(200, 50),
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(4)),
-                              side: BorderSide(color: Colors.black))),
-                      child: const Text("Create Group",
-                          style: TextStyle(color: Colors.black)),
-                    ))
-              ],
-            ),
-          ],
-        ));
+      ),
+    );
   }
 }
